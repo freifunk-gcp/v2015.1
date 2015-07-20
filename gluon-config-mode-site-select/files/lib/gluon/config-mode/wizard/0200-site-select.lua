da97b57cc410383a84623fece7862b17d981c863
Hendrik Lüth <hendrik@linux-nerds.de>
2015-07-20 08:30:35 +0200

    remove the change-key option

--- gluon-config-mode-site-select/files/lib/gluon/config-mode/wizard/0200-site-select.lua
+++ gluon-config-mode-site-select/files/lib/gluon/config-mode/wizard/0200-site-select.lua
@@ -38,23 +38,7 @@ end
 function M.handle(data)
 
 	if data.community ~= site.site_code then
-		uci:set('siteselect', site.site_code, "secret", uci:get('fastd', 'mesh_vpn', 'secret'))
-		uci:save('siteselect')
-		uci:commit('siteselect')
-
-		uci:delete('fastd', 'mesh_vpn', 'secret')
-
-		local secret = uci:get('siteselect', data.community, 'secret')
 		
-		if not secret or not secret:match(("%x"):rep(64)) then
-			uci:delete('siteselect', data.community, 'secret')
-		else
-			uci:set('fastd', 'mesh_vpn', "secret", secret)
-		end
-				
-		uci:save('fastd')
-		uci:commit('fastd')
-
 		fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
 		
 		os.execute('sh "/lib/gluon/site-upgrade"')
--- gluon-luci-site-select/files/usr/lib/lua/luci/model/cbi/admin/site-select.lua
+++ gluon-luci-site-select/files/usr/lib/lua/luci/model/cbi/admin/site-select.lua
@@ -35,26 +35,12 @@ end
 function f.handle(self, state, data)
 	if state == FORM_VALID then
 
-		uci:set('siteselect', site.site_code, "secret", uci:get('fastd', 'mesh_vpn', 'secret'))
-		uci:save('siteselect')
-		uci:commit('siteselect')
-
-		uci:delete('fastd', 'mesh_vpn', 'secret')
-
-		local secret = uci:get('siteselect', data.community, 'secret')
+		if data.community ~= site.site_code then
 		
-		if not secret or not secret:match(("%x"):rep(64)) then
-			uci:delete('siteselect', data.community, 'secret')
-		else
-			uci:set('fastd', 'mesh_vpn', "secret", secret)
+			fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
+			
+			os.execute('sh "/lib/gluon/site-upgrade"')
 		end
-				
-		uci:save('fastd')
-		uci:commit('fastd')
-
-		fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
-		
-		os.execute('sh "/lib/gluon/site-upgrade"')
 	end
 end
 
